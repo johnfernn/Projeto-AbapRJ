714e187cdda0bee7a4c4bad9ebce55bc28c71df9
pedro <pedrocezar@soulasalle.com.br>
2024-05-19 05:31:13 -0300

    mudanças do Back-ende,mesclando e criando mecanicas para melhor navergação e seguraça. feito por: (Stefany e Pedro)

---
+++ Setup_Principal_ABAPRJ/static_root/js/script.js
@@ -0,0 +1,74 @@
+// Carrosel
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 4){
+        cont = 1
+    }
+
+    document.getElementById('radio' + cont).checked = true
+}
+
+//Sobre o projeto
+var botao = document.getElementById('read_button')
+
+botao.addEventListener('click', function(){
+    var card = document.querySelector('.sobreOProjeto')
+    var imagem = document.getElementById('iconeSeta')
+    var texto = document.getElementById('descritivo')
+    card.classList.toggle('active')
+
+    if(imagem.src.includes('imagens/SetaParaBaixo.svg')){
+        imagem.src = 'imagens/SetaParaCima.svg'
+        imagem.alt = 'Seta para cima'
+    } else {
+        imagem.src = 'imagens/SetaParaBaixo.svg'
+        imagem.alt = 'Seta para baixo'
+    }
+
+    if(texto.textContent == 'Leia mais'){
+        texto.textContent = 'Leia menos'
+    } else {
+        texto.textContent = 'Leia mais'
+    }
+})
+
+
+//Crianças
+var btn1 = document.getElementById('imgCriancaPQ1')
+var btn2 = document.getElementById('imgCriancaPQ2')
+var btn3 = document.getElementById('imgCriancaPQ3')
+var btn4 = document.getElementById('imgCriancaPQ4')
+
+function tracaImagem1(){
+    var imagemGrande = document.getElementById('imagemCriancaGrande')
+
+    imagemGrande.src = '/static/imagens/menu01.jpg'
+}
+
+function tracaImagem2(){
+    var imagemGrande = document.getElementById('imagemCriancaGrande')
+
+    imagemGrande.src = '/static/imagens/menu02.jpg'
+}
+
+function tracaImagem3(){
+    var imagemGrande = document.getElementById('imagemCriancaGrande')
+
+    imagemGrande.src = '/static/imagens/menu03.jpg'
+}
+
+function tracaImagem4(){
+    var imagemGrande = document.getElementById('imagemCriancaGrande')
+
+    imagemGrande.src = '/static/imagens/menu04.jpg'
+}
